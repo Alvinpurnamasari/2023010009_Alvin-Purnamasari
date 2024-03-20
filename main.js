3e62201af552acfd8a8bbff2bcf48b873e10eeb3
Alvinpurnamasari <162394212+Alvinpurnamasari@users.noreply.github.com>
2024-03-20 12:31:31 +0700

    firstcome

--- main.js
+++ main.js
@@ -6,11 +6,22 @@ let pacar = null
 
 beratBadan = 49
 
-if(pacar == null){
-    pacar = 'belum punya'
-}else{
-    pacar = 'udah punya'
-}
+// if(pacar == null){
+//  pacar = 'belum punya'
+// }else{
+//    pacar = 'udah punya'
+// }
 
+switch(pacar) {
+    case 1:
+        pacar = 'punya 1 '
+        break
+    case 2:
+        pacar = 'punya pacar 2 aku cukup play girl'
+        break
+    default :
+        pacar = 'belum punya'
+        break
+}
 alert(`nama saya ${nama} usia saya itu ${usia} th tinggi badan saya adalah ${tinggiBadan} cm berat badan saya ${beratBadan} kg dan pacar saya ${pacar}`)
 
