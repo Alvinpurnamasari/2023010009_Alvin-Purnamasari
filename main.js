400bc090df3be6dd52f1b671e4d1acca2792dc21
Alvinpurnamasari <162394212+Alvinpurnamasari@users.noreply.github.com>
2024-03-20 12:31:13 +0700

    firstcome

---
+++ main.js
@@ -0,0 +1,16 @@
+let nama = 'alvin purnama sari' //tipe string
+let usia = 18 // tipe integer number
+let tinggiBadan = 156 //tipe double float
+let beratBadan 
+let pacar = null
+
+beratBadan = 49
+
+if(pacar == null){
+    pacar = 'belum punya'
+}else{
+    pacar = 'udah punya'
+}
+
+alert(`nama saya ${nama} usia saya itu ${usia} th tinggi badan saya adalah ${tinggiBadan} cm berat badan saya ${beratBadan} kg dan pacar saya ${pacar}`)
+
